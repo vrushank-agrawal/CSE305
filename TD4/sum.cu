6f570d0507b0f87c865d5384604231eaa47ce81c
vrushank-agrawal <vrushank2001@gmail.com>
2023-04-19 00:28:06 +0200

    implement TD4

--- TD4/sum.cu
+++ TD4/sum.cu
@@ -2,6 +2,15 @@
 #include <iostream>
 #include <chrono>
 #include <math.h>
+#include <cfloat>
+#include <climits>
+#include <thread>
+#include <numeric>
+#include <iterator>
+#include <vector>
+#include <atomic>
+#include <cmath>
+#include <mutex>
 
 //------------------------------------------------
 
@@ -15,6 +24,42 @@ double Sum(double* arr, size_t N) {
 
 //-------------------------------------------------
 
+void SumMapThread(double* arr, size_t begin, size_t end, double& result) {
+    while (begin != end)
+        result += arr[begin++];
+}
+
+void SumParallel(double* result, size_t N, size_t num_threads) {
+    size_t block_size = N / num_threads;
+    std::vector<double> results(num_threads, 0.0);
+
+    std::vector<std::thread> workers(num_threads - 1);
+    for (size_t i = 0; i < num_threads - 1; i++)
+        workers[i] = std::thread(&SumMapThread,
+                                result,
+                                (i*block_size),
+                                ((i+1)*block_size),
+                                std::ref(results[i]));
+    SumMapThread(result, (num_threads-1)*block_size, N, results[num_threads - 1]);
+
+    for (auto& t : workers) t.join();
+    result[0] = std::accumulate(results.begin(), results.end(), 0.0);
+}
+
+__global__ void PartialSumGPUAux(double* arr, double* partial_sums, size_t N, size_t chunk_size) {
+    size_t index = blockIdx.x * blockDim.x + threadIdx.x;
+    size_t begin = chunk_size * index;
+    size_t end = chunk_size * (index + 1);
+    double sum = 0.;
+
+    if (end > N)
+        end = N;
+
+    for (size_t i = begin; i < end; ++i)
+        sum += arr[i];
+
+    partial_sums[index] = sum;
+}
 
 /**
  * @brief Computes the sum of the array
@@ -22,7 +67,36 @@ double Sum(double* arr, size_t N) {
  * @param N - the length of the array
  */
 double SumGPU(double* arr, size_t N) {
-    return 0.;
+    // implement sum of the array on GPU using cuda
+    const size_t BLOCKS_NUM = 64;
+    const size_t THREADS_PER_BLOCK = 256;
+    const size_t TOTAL_THREADS = BLOCKS_NUM  * THREADS_PER_BLOCK;
+
+    // moving the data to device
+    double* result = new double[TOTAL_THREADS];
+    double* arrGPU;
+    double* partialResGPU;
+    cudaMalloc(&arrGPU, N * sizeof(double));
+    cudaMalloc(&partialResGPU, TOTAL_THREADS * sizeof(double));
+    cudaMemcpy(arrGPU, arr, N * sizeof(double), cudaMemcpyHostToDevice);
+
+    // computing on GPU
+    size_t chunk_size = (N + TOTAL_THREADS + 1) / TOTAL_THREADS;
+    PartialSumGPUAux<<<BLOCKS_NUM, THREADS_PER_BLOCK>>>(arrGPU, partialResGPU, N, chunk_size);
+    cudaDeviceSynchronize();
+
+    // copying the result back
+    cudaMemcpy(result, partialResGPU, TOTAL_THREADS * sizeof(double), cudaMemcpyDeviceToHost);
+
+    // Free memory
+    cudaFree(arrGPU);
+    cudaFree(partialResGPU);
+
+    SumParallel(result, TOTAL_THREADS, 8);
+    double res = result[0];
+    delete[] result;
+
+    return res;
 }
 
 //---------------------------------------------------
@@ -40,11 +114,11 @@ int main(int argc, char* argv[]) {
     for (size_t i = 0; i < N; ++i) {
           arr[i] = static_cast <double> (rand()) / static_cast <double> (RAND_MAX);
     }
- 
+
     double result = 0.;
     auto start = std::chrono::steady_clock::now();
     switch (alg_ind) {
-        case 0: 
+        case 0:
             result = Sum(arr, N);
             break;
         case 1:
@@ -52,10 +126,11 @@ int main(int argc, char* argv[]) {
             break;
     }
     auto finish = std::chrono::steady_clock::now();
-    auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(finish - start).count(); 
+    auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(finish - start).count();
     std::cout << "Elapsed time: " << elapsed << std::endl;
     std::cout << "Total result: " << result << std::endl;
-    
+
     delete[] arr;
     return 0;
 }
+
--- TD4/sum_distances.cu
+++ TD4/sum_distances.cu
@@ -2,9 +2,17 @@
 #include <iostream>
 #include <chrono>
 #include <math.h>
+#include <cfloat>
+#include <climits>
+#include <thread>
+#include <numeric>
+#include <iterator>
+#include <vector>
+#include <atomic>
+#include <cmath>
+#include <mutex>
 
-__device__
-double DistKer(double* p, double* q, size_t dim) {
+__device__ double DistKer(double* p, double* q, size_t dim) {
     double result = 0;
     for (size_t i = 0; i < dim; ++i) {
         result += (p[i] - q[i]) * (p[i] - q[i]);
@@ -35,6 +43,42 @@ double SumDistances(double* arr, size_t dim, size_t N) {
 
 //-------------------------------------------------
 
+void SumMapThread(double* arr, size_t begin, size_t end, double& result) {
+    while (begin != end)
+        result += arr[begin++];
+}
+
+void SumParallel(double* result, size_t N, size_t num_threads) {
+    size_t block_size = N / num_threads;
+    std::vector<double> results(num_threads, 0.0);
+
+    std::vector<std::thread> workers(num_threads - 1);
+    for (size_t i = 0; i < num_threads - 1; i++)
+        workers[i] = std::thread(&SumMapThread,
+                                result,
+                                (i*block_size),
+                                ((i+1)*block_size),
+                                std::ref(results[i]));
+    SumMapThread(result, (num_threads-1)*block_size, N, results[num_threads - 1]);
+
+    for (auto& t : workers) t.join();
+    result[0] = std::accumulate(results.begin(), results.end(), 0.0);
+}
+
+__global__ void SumDistancesGPUAux(double* arr, size_t dim, size_t N, double* result) {
+    size_t curr_pt = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (curr_pt >= N) return;
+
+    double* p = arr + (curr_pt * dim);
+    double sum = 0;
+    for (size_t i = 0; i < N; ++i) {
+        if (i == curr_pt) continue;
+        sum += DistKer(p, arr + (i * dim), dim);
+    }
+
+    result[curr_pt] = sum;
+}
 
 /**
  * @brief Computes the sum of pairwise distances between the points
@@ -45,7 +89,30 @@ double SumDistances(double* arr, size_t dim, size_t N) {
  */
 
 double SumDistancesGPU(double* arr, size_t dim, size_t N) {
-    return 0.;
+    const size_t block_size = 64;
+    const size_t threads_per_block = 512;
+    const size_t total_threads = block_size * threads_per_block;
+
+    double* result = new double[N];
+    double* arrGPU;
+    double* resultGPU;
+    cudaMalloc(&arrGPU, N * dim * sizeof(double));
+    cudaMalloc(&resultGPU, N * sizeof(double));
+    cudaMemcpy(arrGPU, arr, N * dim * sizeof(double), cudaMemcpyHostToDevice);
+
+    for (size_t i = 0; i < N; i += total_threads)
+        SumDistancesGPUAux<<<block_size, threads_per_block>>>(arrGPU + (i * dim * total_threads), dim, N, resultGPU + (i * total_threads));
+    cudaDeviceSynchronize();
+
+    cudaMemcpy(result, resultGPU, N * sizeof(double), cudaMemcpyDeviceToHost);
+
+    cudaFree(arrGPU);
+    cudaFree(resultGPU);
+
+    SumParallel(result, N, 8);
+    double sum = result[0];
+    delete[] result;
+    return sum/2;
 }
 
 //---------------------------------------------------
@@ -64,11 +131,11 @@ int main(int argc, char* argv[]) {
     for (size_t i = 0; i < dim * N; ++i) {
           arr[i] = static_cast <double> (rand()) / static_cast <double> (RAND_MAX);
     }
- 
+
     double result = 0.;
     auto start = std::chrono::steady_clock::now();
     switch (alg_ind) {
-        case 0: 
+        case 0:
             result = SumDistances(arr, dim, N);
             break;
         case 1:
@@ -76,10 +143,10 @@ int main(int argc, char* argv[]) {
             break;
     }
     auto finish = std::chrono::steady_clock::now();
-    auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(finish - start).count(); 
+    auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(finish - start).count();
     std::cout << "Elapsed time: " << elapsed << std::endl;
     std::cout << "Total result: " << result << std::endl;
-    
+
     delete[] arr;
     return 0;
 }
