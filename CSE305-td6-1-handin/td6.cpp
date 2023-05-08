194e490162c0fd131cc20184465f4be9b7e26c9f
vrushank-agrawal <vrushank2001@gmail.com>
2023-05-08 18:09:32 +0200

    complete lab6

--- CSE305-td6-1-handin/td6.cpp
+++ CSE305-td6-1-handin/td6.cpp
@@ -1,59 +1,65 @@
 #include <algorithm>
-#include <chrono>
 #include <condition_variable>
-#include <functional>
 #include <future>
-#include <iostream>
-#include <cmath>
 #include <mutex>
-#include <numeric>
 #include <thread>
 #include <queue>
 
 //----------------------------------------------------------------------------
-
 class OrderedVec {
-        std::vector<int> data;
-        std::condition_variable cv;
-        std::mutex m;
-        std::mutex search_m;
-        // You can add some mutexes/atomics here is you want
-    public:
-        OrderedVec() {
+    std::vector<int> data;
+    std::mutex m;
+    std::atomic<bool> inserting{false};
+
+public:
+    void insert(int val) {
+        // Set inserting to true
+        inserting.store(true, std::memory_order_release);
+
+        std::lock_guard<std::mutex> lock(m);
+        if (data.empty()) {
+            data.push_back(val);
+        } else {
+            // if the vector is not sorted, sort it
+            if (!std::is_sorted(data.begin(), data.end()))
+                std::sort(data.begin(), data.end());
+
+            // find the first element that is greater than val and insert val before it
+            auto greater_val = std::upper_bound(data.begin(), data.end(), val);
+            data.insert(greater_val, val);
         }
 
-        void insert(int val) {
-            std::lock_guard<std::mutex> lock(m);
-            cv.wait(lock, [this](){return true;});
-            if (data.empty())
-                data.push_back(val);
-            else
-                for (auto& v : data)
-                    if (v > val) {
-                        // insert the data and extend the vector
-                        data.insert(v, val);
-                        break;
-                    }
-            cv.notify_all();
-        }
+        // Set inserting to false
+        inserting.store(false, std::memory_order_release);
+    }
 
-        bool search(int val) {
-            std::lock_guard<std::mutex> lock(search_m);
-            cv.wait(lock, [this](){return true;});
-            if (std::find(data.begin(), data.end(), val) != data.end())
-                return true;
+    bool search(int val) {
+        // If an insertion is happening don't start a search
+        if (inserting.load(std::memory_order_acquire))
             return false;
-        }
 
-        // used for testing
-        std::vector<int> get_data() {
-            return data;
-        }
+        // Search the data
+        std::lock_guard<std::mutex> lock(m);
+        if (std::find(data.begin(), data.end(), val) != data.end())
+            return true;
+
+        return false;
+    }
+
+    // used for testing
+    std::vector<int> get_data() {
+        return data;
+    }
 };
 
 //----------------------------------------------------------------------------
 
 void DivideOnceEven(std::condition_variable& iseven, std::mutex& m, int& n) {
+    std::unique_lock<std::mutex> lock(m);
+    while (n % 2 != 0)
+        iseven.wait(lock);
+    n /= 2;
+    iseven.notify_all();
 }
 
 //-----------------------------------------------------------------------------
@@ -72,10 +78,18 @@ class SafeUnboundedQueue {
 
 template <class E>
 void SafeUnboundedQueue<E>::push(const E& element) {
+    std::unique_lock<std::mutex> lock(this->lock);
+    this->elements.push(element);
+    if (is_empty()) this->not_empty.notify_all();
 }
 
 template <class E>
 E SafeUnboundedQueue<E>::pop() {
+    std::unique_lock<std::mutex> lock(this->lock);
+    this->not_empty.wait(lock, [this](){return !this->is_empty();});
+    E element = this->elements.front();
+    this->elements.pop();
+    return element;
 }
 
 //-----------------------------------------------------------------------------
